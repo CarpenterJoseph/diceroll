d769295d35582d78526b5b92e321868770ef29bc
Joe <carpenter.joseph1998@gmail.com>
2020-10-26 13:52:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,15 +13,11 @@ function isDiceRoll(diceRoll) {
 client.on('message', msg => {
 	//if message is from self, prevents infinite loop
 	if (msg.author.bot) return;
-	//content of message
 	const content = msg.content.toLowerCase()
 	//if message contains any letter other than d or number, return
 	if(/[^d0-9]+/.test(content)) return
-	//channel message was sent in
 	const channel = client.channels.cache.get(msg.channel.id)
-	//location of d e.g 2d4 is 1
 	const dLocation = content.search('d')
-	//diceroll with amount of dice and dice sides extracted from message
 	const diceRoll = {
 		diceAmount: content.slice(0, dLocation),
 		diceSides: content.slice(dLocation + 1, content.length)
