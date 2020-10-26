6aadf3fd8e42aa1a6f1b76035b2beb962987000a
Joe <carpenter.joseph1998@gmail.com>
2020-10-26 13:07:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 //if message was a valid diceroll
-function isDiceRoll(diceRoll) {
+function isDiceRoll(diceRoll, content) {
 	if(diceRoll.diceAmount.isNaN || diceRoll.diceSides.isNaN)
 		return false
 	if(diceRoll.diceAmount < 1 || diceRoll.diceSides < 1)
 		return false
-
+	if(!content.contains('d') && !content.contains('D'))
+		return false
 	return true
 }
 
 client.on('message', msg => {
 	//if message is from self, prevents infinite loop
 	if (msg.author.bot) return;
-	//channel message was sent in
-	const channel = client.channels.cache.get(msg.channel.id)
-
 	//content of message
 	const content = msg.content
+	//channel message was sent in
+	const channel = client.channels.cache.get(msg.channel.id)
 	//location of d e.g 2d4 is 1
 	const dLocation = content.search('d')
 	//diceroll with amount of dice and dice sides extracted from message
@@ -27,7 +27,7 @@ client.on('message', msg => {
 		diceSides: content.substr(dLocation + 1, content.length)
 	}
 
-	if(isDiceRoll(diceRoll)){
+	if(isDiceRoll(diceRoll, content)){
 		let results = []
 		let total = 0
 		let returnMessage = '\`\`\`\n'
